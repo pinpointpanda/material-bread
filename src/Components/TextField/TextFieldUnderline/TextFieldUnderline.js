f0f3e1ae5248e905db9e79b40272c6e63a9a7c4e
Mike Ward <mike@pinpointpanda.com>
2020-06-03 17:26:59 +0100

    Temp icnrease anim

--- src/Components/TextField/TextFieldUnderline/TextFieldUnderline.js
+++ src/Components/TextField/TextFieldUnderline/TextFieldUnderline.js
@@ -19,7 +19,7 @@ class TextFieldUnderline extends Component {
 
   state = {
     underLineXAnimation: new Animated.Value(0),
-    animationDuration: 200, //testcommentmore
+    animationDuration: 2000, //testcommentmore
     animationEasing: Easing.ease,
   };
 
